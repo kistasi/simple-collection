facc688a2f63288e0931103a93ec0b638dc51215
Marton Tasnadi <marton.tasnadi@gmail.com>
2020-12-30 16:21:24 +0100

    Add new features

--- src/Collection.php
+++ src/Collection.php
@@ -2,16 +2,23 @@
 
 namespace SimpleCollection;
 
+require './vendor/autoload.php';
+
 class Collection
 {
     private $collection;
 
-    public function __construct(array $array)
+    public function __construct(?array $array = [])
     {
         $this->collection = $array;
     }
 
-    public function append($value)
+    public function get($value)
+    {
+        return $this->collection[$value];
+    }
+
+    public function append($value): array
     {
         $collection = $this->collection;
 
@@ -22,7 +29,66 @@ class Collection
         return $this->collection;
     }
 
-    public function toArray()
+    public function appendMultiple(array $values): array
+    {
+        $collection = $this->collection;
+
+        $this->collection = array_merge($collection, $values);
+
+        return $this->collection;
+    }
+
+    public function prepend($value): array
+    {
+        $collection = $this->collection;
+
+        array_unshift($collection, $value);
+
+        $this->collection = $collection;
+
+        return $this->collection;
+    }
+
+    public function prependMultiple(array $values)
+    {
+        $collection = $this->collection;
+
+        $values = array_reverse($values);
+
+        foreach ($values as $value) {
+            array_unshift($collection, $value);
+        }
+
+        $this->collection = $collection;
+
+        return $this->collection;
+    }
+
+    public function remove(string $value): array
+    {
+        $collection = $this->collection;
+
+        unset($collection[array_search($value, $collection)]);
+
+        $this->collection = $collection;
+
+        return $this->collection;
+    }
+
+    public function removeMultiple(array $values): array
+    {
+        $collection = $this->collection;
+
+        foreach ($values as $value) {
+            unset($collection[array_search($value, $collection)]);
+        }
+
+        $this->collection = array_values($collection);
+
+        return $this->collection;
+    }
+
+    public function toArray(): array
     {
         return $this->collection;
     }
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -1,15 +1,74 @@
 <?php
 
-require_once './vendor/autoload.php';
-
 use PHPUnit\Framework\TestCase;
 
 class CollectionTest extends TestCase
 {
+    public function testToArray()
+    {
+        $collection = new SimpleCollection\Collection();
+        $this->assertSame([], $collection->toArray());
+
+        $collection2 = new SimpleCollection\Collection([]);
+        $this->assertSame([], $collection2->toArray());
+
+        $collection3 = new SimpleCollection\Collection(['a', 'b', 'c']);
+        $this->assertSame(['a', 'b', 'c'], $collection3->toArray());
+    }
+
+    public function testGet()
+    {
+        $collection = new SimpleCollection\Collection(['name' => 'Robin', 'age' => 29]);
+
+        $this->assertSame('Robin', $collection->get('name'));
+        $this->assertSame(29, $collection->get('age'));
+    }
+
     public function testAppend()
     {
-        $collection = new SimpleCollection\Collection(['asd']);
-        $collection->append('dsa');
-        $this->assertSame(['asd', 'dsa'], $collection->toArray());
+        $collection = new SimpleCollection\Collection(['a']);
+        $collection->append('b');
+
+        $this->assertSame(['a', 'b'], $collection->toArray());
+    }
+
+    public function testAppendMultiple()
+    {
+        $collection = new SimpleCollection\Collection();
+        $collection->appendMultiple(['a', 'b', 'c']);
+
+        $this->assertSame(['a', 'b', 'c'], $collection->toArray());
+    }
+
+    public function testPrepend()
+    {
+        $collection = new SimpleCollection\Collection(['a', 'b']);
+        $collection->prepend('c');
+
+        $this->assertSame(['c', 'a', 'b'], $collection->toArray());
+    }
+
+    public function testPrependMultiple()
+    {
+        $collection = new SimpleCollection\Collection(['a', 'b', 'c']);
+        $collection->prependMultiple(['d', 'e']);
+
+        $this->assertSame(['d', 'e', 'a', 'b', 'c'], $collection->toArray());
+    }
+
+    public function testRemove()
+    {
+        $collection = new SimpleCollection\Collection(['a', 'b']);
+        $collection->remove('b');
+
+        $this->assertSame(['a'], $collection->toArray());
+    }
+
+    public function testRemoveMultiple()
+    {
+        $collection = new SimpleCollection\Collection(['a', 'b', 'c']);
+        $collection->removeMultiple(['a', 'b']);
+
+        $this->assertSame(['c'], $collection->toArray());
     }
 }
